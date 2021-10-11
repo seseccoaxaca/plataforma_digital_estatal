2c12a7ed91828a4faea9dcb525357762a400e9e6
MemoRT <guillermo.ramirezt15@anahuac.mx>
2021-10-11 13:33:22 -0500

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -43,11 +43,12 @@ export default function AlertDialogSlide() {
             <br/>
             <ul>
               <li>Contiene datos reales de los Sistemas: 1, 2, 4. </li>
+              <br/>
               <li>Contiene nuevas secciones y funcionalidades.</li>
+              <br/>
               <li>La PDE se construye de manera progresiva, modular y escalable. Como lo establece el acuerdo mediante el cual el Comité Coordinador del Sistema Estatal de combate a la Corrupción emita el análisis para la implementación y Operación de la Plataforma Digital Estatal y las <a href="http://www.periodicooficial.oaxaca.gob.mx/files/2021/01/SEC03-03RA-2021-01-16.pdf">Bases para el funcionamiento de la Plataforma Digital Estatal</a> de acuerdo al artículo 18 , 22 , 29 , 31 , 32. La Plataforma tendrá constantes actualizaciones y mejoras, por lo que esta versión NO debe ser vista como la última. </li>
 
             </ul>
-            <br/>
             Todos los usuarios de la PDE deberán privilegiar los intereses de las personas titulares de los datos personales contenidos en los sistemas. El tratamiento de los datos consultables en la PDE no debe utilizarse para ningún tipo de discriminación, trato injusto, arbitrario o contrario a lo establecido en la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados y la local.
             <br/>Te invitamos a revisar los <a href="http://www.periodicooficial.oaxaca.gob.mx/files/2021/01/SEC03-03RA-2021-01-16.pdf">"Términos y Condiciones de Uso".</a>
 
