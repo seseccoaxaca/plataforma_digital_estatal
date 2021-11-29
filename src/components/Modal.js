c19b4e7d99e0eab5f8cee691e5019ef45c081bff
Jair Gómez Vásquez <36084661+Jairgo@users.noreply.github.com>
2021-11-28 19:31:31 -0600

    Previo merge

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -61,7 +61,6 @@ export default function AlertDialogSlide() {
               <li>Contiene nuevas secciones y funcionalidades.</li>
               <br/>
               <li>La PDE se construye de manera progresiva, modular y escalable. Como lo establece el acuerdo mediante el cual el Comité Coordinador del Sistema Estatal de combate a la Corrupción emita el análisis para la implementación y Operación de la Plataforma Digital Estatal y las <a rel="noopener" rel="noreferrer" target="_blank" href="http://www.periodicooficial.oaxaca.gob.mx/files/2021/01/SEC03-03RA-2021-01-16.pdf">Bases para el funcionamiento de la Plataforma Digital Estatal</a> de acuerdo al artículo 18 , 22 , 29 , 31 , 32. La Plataforma tendrá constantes actualizaciones y mejoras, por lo que esta versión NO debe ser vista como la última. </li>
-
             </ul>
             Todos los usuarios de la PDE deberán privilegiar los intereses de las personas titulares de los datos personales contenidos en los sistemas. El tratamiento de los datos consultables en la PDE no debe utilizarse para ningún tipo de discriminación, trato injusto, arbitrario o contrario a lo establecido en la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados y la local.
             <br/>Te invitamos a revisar los <a rel="noopener" rel="noreferrer"  target="_blank" href="http://www.periodicooficial.oaxaca.gob.mx/files/2021/01/SEC03-03RA-2021-01-16.pdf">"Términos y Condiciones de Uso".</a>
@@ -113,51 +112,3 @@ export default function AlertDialogSlide() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import "./modal.css";
-
-// function Modal({ setOpenModal }) {
-//   return (
-//     <div className="modalBackground">
-//       <div className="modalContainer">
-//         <div className="titleCloseBtn">
-//           <button
-//             onClick={() => {
-//               setOpenModal(false);
-//             }}
-//           >
-//             X
-//           </button>
-//         </div>
-//         <div className="title">
-//           <h1>Are You Sure You Want to Continue?</h1>
-//         </div>
-//         <div className="body">
-//           <p>The next page looks amazing. Hope you want to go there!</p>
-//         </div>
-//         <div className="footer">
-//           <button
-//             onClick={() => {
-//               setOpenModal(false);
-//             }}
-//             id="cancelBtn"
-//           >
-//             Cancel
-//           </button>
-//           <button>Continue</button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Modal;
